3245767e1376f3bbf5d53b621dd32f1d0576fd21
Chuudim <chulanovdima2507@gmail.com>
2023-03-16 18:53:51 +0200

    ДЗ нью

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,16 @@
-
-import Vimeo from '@vimeo/player';
+import * as Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
-const player = new Vimeo.Player(iframe);
+const player = new Vimeo.default(iframe);
 
 player.on('timeupdate', throttle(function(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 }, 1000));
 
-const currentTime = localStorage.getItem('videoplayer-current-time');
-if (currentTime) {
-  player.setCurrentTime(currentTime);
-}
+player.on('loaded', function() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,10 @@
-import throttle from 'lodash.throttle';
-
+import throttle from 'lodash/throttle';
 
 const form = document.querySelector('.feedback-form');
 const emailField = form.querySelector('[name="email"]');
 const messageField = form.querySelector('[name="message"]');
 
-
 const savedState = JSON.parse(localStorage.getItem('feedback-form-state'));
-
 
 if (savedState) {
   emailField.value = savedState.email;
@@ -15,7 +12,7 @@ if (savedState) {
 }
 
 
-const updateState = _.throttle(() => {
+const updateState = throttle(() => {
   const state = {
     email: emailField.value,
     message: messageField.value,
@@ -23,19 +20,17 @@ const updateState = _.throttle(() => {
   localStorage.setItem('feedback-form-state', JSON.stringify(state));
 }, 500);
 
-
 emailField.addEventListener('input', updateState);
 messageField.addEventListener('input', updateState);
-
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const state = {
-    email: '',
-    message: '',
+    email: emailField.value,
+    message: messageField.value,
   };
   localStorage.setItem('feedback-form-state', JSON.stringify(state));
+  console.log({ email: state.email, message: state.message });
   emailField.value = '';
   messageField.value = '';
-  console.log({ email: state.email, message: state.message });
 });
